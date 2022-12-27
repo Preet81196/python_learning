3c3460cfe615beb723ceb59d461d1a8963904451
Preet81196 <31180792+Preet81196@users.noreply.github.com>
2022-12-26 21:53:22 -0600

    codewars

--- Codewars/ary.py
+++ Codewars/ary.py
@@ -1,4 +1,10 @@
+'''find the nearest square number, nearest_sq(n), of a positive integer n'''
+def nearest_sq(n):
+    return round(n ** 0.5) ** 2
 
 
 if __name__ == '__main__':
+    print(nearest_sq(10))
+    print(nearest_sq(111))
+    print(nearest_sq(9999))
 
