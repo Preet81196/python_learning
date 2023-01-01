57e9296e074031d4da15f3d84cb0658e4e6b288a
Preet81196 <31180792+Preet81196@users.noreply.github.com>
2023-01-01 05:42:38 -0600

    .

---
+++ Codewars/who_like_it_list.py
@@ -0,0 +1,13 @@
+def likes(names):
+    if len(names) == 0:
+        return
+    elif len(names) == 1:
+        return names[0] + " likes this"
+    elif len(names) == 2:
+        return names[0] + " and " + names[1] + " like this"
+    elif len(names) == 3:
+        return names[0] + ", " + names[1] + " and " + names[2] + " like this"
+    else:
+        return names[0] + ", " + names[1] + " and " + str(len(names)-2) + " others like this"
+
+print(likes(['preet','sagar','divyesh','patel']))
