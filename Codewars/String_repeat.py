c15ef9f35ea505d68b0ca10bcc495123273f2e00
Preet81196 <31180792+Preet81196@users.noreply.github.com>
2023-01-04 07:02:47 -0600

    String repeat

---
+++ Codewars/String_repeat.py
@@ -0,0 +1,7 @@
+def repeat_str(repeat, string):
+    return string * repeat
+
+if __name__ == '__main__':
+    print(repeat_str(4, 'a'))
+    print(repeat_str(3, 'hello '))
+    print(repeat_str(2, 'abc'))
