b05b6c90d2b0f4c0ce5fb4f7f0d80456af836f41
Preet81196 <31180792+Preet81196@users.noreply.github.com>
2023-01-10 07:30:55 -0600

    power of 2

---
+++ Codewars/power_of_2.py
@@ -0,0 +1,9 @@
+#Complete the function that takes a non-negative integer n as input,
+#and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).'''
+
+def powers_of_two(n):
+    return [2**i for i in range(n+1)]
+
+
+print(powers_of_two(0))
+print(powers_of_two(1))
