b5f2d3f3f8da08a258203b2c411998af3b5091ca
Preet81196 <31180792+Preet81196@users.noreply.github.com>
2023-01-17 08:02:12 -0600

    sum of odd num

---
+++ Codewars/sum_of_odd_triangle.py
@@ -0,0 +1,9 @@
+'''Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: 
+(Input --> Output)
+1 -->  1
+2 --> 3 + 5 = 8'''
+
+
+
+def row_sum_odd_numbers(n):
+    return n**3
